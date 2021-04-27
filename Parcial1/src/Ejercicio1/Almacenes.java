8f388691962e6601d5be4a3f6295fe5682ce42ca
Josué Morón <josuemoron@upeu.edu.pe>
2021-04-27 18:27:53 -0500

    Creacion e implementacion del ejercicio 1

---
+++ Parcial1/src/Ejercicio1/Almacenes.java
@@ -0,0 +1,16 @@
+package Ejercicio1;
+
+public class Almacenes {
+	private int numProductos;
+    public Almacenes(int nProductos){
+            this.numProductos=nProductos;
+    }
+    public boolean cogerProducto(){
+            if (this.numProductos>0){
+                    this.numProductos--;
+                    return true;
+            }
+            return false;
+    }
+
+}
